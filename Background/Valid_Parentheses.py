2b6bf14bb1981bc4c5a9846e3adf9e5748a264e9
BananMoon <myj7082@naver.com>
2021-06-26 23:11:00 +0900

    leetcode_n20

--- Background/Valid_Parentheses.py
+++ Background/Valid_Parentheses.py
@@ -8,11 +8,11 @@ class Solution:
             '}':'{'
         }
         for char in s:
-             if char not in table: # 닫는 괄호가 아니면 stack에 추가
-                    stack.append(char)
-            eiif not char or table(char) != stack.pop():  # stack이 비어있거나, 마지막 원소가 일치하는 괄호가 아니라면
+            if char not in table: # 닫는 괄호가 아니면 stack에 추가
+                stack.append(char)
+            elif not stack or table[char] != stack.pop(): # stack이 비어있거나, 마지막 원소가 일치하는 괄호가 아니라면
                 return False
-            return len(stack) == 0  # stack이 비었으면 True 리턴
+        return len(stack) == 0  # stack이 비었으면 True 리턴
         
         # 예) (([{}]))
         # (, (, [, { append
